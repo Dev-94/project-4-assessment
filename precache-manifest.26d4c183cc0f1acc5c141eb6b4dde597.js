a30fc9308e20fc2cae1ad55572196d8d98fd27a4
Dev Govindji <friendsofdev94@gmail.com>
2019-10-09 12:22:03 -0500

    Updates

---
+++ precache-manifest.26d4c183cc0f1acc5c141eb6b4dde597.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff583e54f92383b7d3149ba9e5f0a9f8",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "ba680784537288d5d2fa",
+    "url": "/project-4-assessment/static/css/main.5f15f5c6.chunk.css"
+  },
+  {
+    "revision": "adcc6c9126018b755d0a",
+    "url": "/project-4-assessment/static/js/2.25804772.chunk.js"
+  },
+  {
+    "revision": "ba680784537288d5d2fa",
+    "url": "/project-4-assessment/static/js/main.6c5fbdff.chunk.js"
+  },
+  {
+    "revision": "75de8ec33a8a78802050",
+    "url": "/project-4-assessment/static/js/runtime-main.71652b97.js"
+  }
+]);
